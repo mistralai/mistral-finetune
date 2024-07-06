9ffb10fea533082eba1404d2eb0ed98f672c1564
stefkarmakov <karmakovst@gmail.com>
2024-07-06 14:50:56 +0100

    Changes to fix issue with validation script
    If messages only contain 1 system message, followed by 1 assistant message, the validation script results in a confusing error.
    
    This change adds the system prompt back into the messages for the validation step to fix this issue and return True if the structure
    of the messages is correct.

--- finetune/data/tokenize.py
+++ finetune/data/tokenize.py
@@ -124,6 +124,7 @@ def build_instruct_sample(data: Dict[str, Any]) -> TrainingInstructSample:
     ):
         available_tools = _parse_available_tools(data["available_tools"])
 
+    added_system_message = False
     for data_message in data_messages:
         is_tool_call = data_message.get("tool_calls") is not None
 
@@ -170,14 +171,19 @@ def build_instruct_sample(data: Dict[str, Any]) -> TrainingInstructSample:
                 raise MessageFormatError(err, str(data))
 
             system_prompt = content
+            messages.append(SystemMessage(content=content))
+            added_system_message = True
         elif data_message["role"] == "tool":
             assert content is not None
             tool_message = _parse_tool_message(content, data_message)
             messages.append(tool_message)
 
     # validate created messages
+    print(messages)
     validator = MistralRequestValidatorV3(ValidationMode.finetuning)
     validator.validate_messages(messages)
+    if added_system_message:
+        messages = messages[1:]
     validator._validate_tools(available_tools or [])
 
     # whether to train only on last assistant message
